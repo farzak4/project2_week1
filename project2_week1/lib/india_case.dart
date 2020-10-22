d01015abd799d300aa507e7cb2303486c74217fe
farzak4 <fariz.prime@gmail.com>
2020-10-22 15:39:14 +0700

    project 2 selese

--- project2_week1/lib/india_case.dart
+++ project2_week1/lib/india_case.dart
@@ -36,47 +36,106 @@ class India extends StatelessWidget {
               Container(
                 child: Text(
                   "Today Cases : 249",
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 20, color: Colors.blue),
                 ),
               ),
               Container(
                 child: Text(
                   "Death : 249",
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 20, color: Colors.red),
                 ),
               ),
               Container(
                 child: Text(
                   "Today Deaths : 249",
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 20, color: Colors.red),
                 ),
               ),
               Container(
                 child: Text(
-                  "Cases : 249",
-                  style: TextStyle(fontSize: 20),
+                  "Recovered : 249",
+                  style: TextStyle(fontSize: 20, color: Colors.green),
                 ),
               ),
               Container(
                 child: Text(
-                  "Cases : 249",
-                  style: TextStyle(fontSize: 20),
+                  "Active Cases : 249",
+                  style: TextStyle(fontSize: 20, color: Colors.yellow),
                 ),
               ),
               Container(
                 child: Text(
-                  "Cases : 249",
-                  style: TextStyle(fontSize: 20),
+                  "Critical : 249",
+                  style: TextStyle(fontSize: 20, color: Colors.yellow),
                 ),
               ),
               Container(
                 child: Text(
-                  "Cases : 249",
-                  style: TextStyle(fontSize: 20),
+                  "Cases Per Million : 249",
+                  style: TextStyle(fontSize: 20, color: Colors.grey),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(24, 5, 24, 5),
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.black)),
+                child: TextField(
+                  decoration: InputDecoration(hintText: 'Input Country'),
+                ),
+              ),
+              Container(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 24),
+                    width: 150,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text("search"),
+                      color: Colors.pink,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 24),
+                    width: 150,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text("All Information"),
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              )),
+              Container(
+                margin: EdgeInsets.fromLTRB(24, 5, 24, 5),
+                width: double.infinity,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("Update of Srilanka"),
+                  color: Colors.red,
+                ),
+              ),
+              Container(
+                child: Text(
+                  "IMPORTANT",
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              Container(
+                child: Text(
+                  "Search 'south korea' as 'Korea'",
+                  style: TextStyle(color: Colors.black),
                 ),
               ),
             ],
-          )
+          ),
         ],
       ),
     );
